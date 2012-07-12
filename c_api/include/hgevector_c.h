3346cc0423f8885782f85b637b54448c0538e808
Harley Laue <losinggeneration@gmail.com>
2012-07-12 13:17:14 -0500

    Didn't prostfix HGE_Vector struct with _s or _t

--- c_api/include/hgevector_c.h
+++ c_api/include/hgevector_c.h
@@ -9,36 +9,36 @@ extern "C" {
 
 float HGE_InvSqrt(float x);
 
-typedef struct HGE_Vector HGE_Vector;
+typedef struct HGE_Vector_s HGE_Vector_t;
 
-float HGE_Vector_GetX(HGE_Vector *v);
-float HGE_Vector_GetY(HGE_Vector *v);
-void HGE_Vector_SetX(HGE_Vector *v, float x);
-void HGE_Vector_SetY(HGE_Vector *v, float y);
+float HGE_Vector_GetX(HGE_Vector_t *v);
+float HGE_Vector_GetY(HGE_Vector_t *v);
+void HGE_Vector_SetX(HGE_Vector_t *v, float x);
+void HGE_Vector_SetY(HGE_Vector_t *v, float y);
 
-HGE_Vector HGE_Vector_Create(float x, float y);
-HGE_Vector HGE_Vector_CreateEmpty();
+HGE_Vector_t HGE_Vector_Create(float x, float y);
+HGE_Vector_t HGE_Vector_CreateEmpty();
 
-HGE_Vector HGE_Vector_Negate(const HGE_Vector v);
-HGE_Vector HGE_Vector_Subtract(const HGE_Vector v1, const HGE_Vector v2);
-HGE_Vector HGE_Vector_Add(const HGE_Vector v1, const HGE_Vector v2);
-HGE_Vector HGE_Vector_Multiply(const HGE_Vector v1, float scalar);
-HGE_Vector HGE_Vector_Divide(const HGE_Vector v1, float scalar);
+HGE_Vector_t HGE_Vector_Negate(const HGE_Vector_t v);
+HGE_Vector_t HGE_Vector_Subtract(const HGE_Vector_t v1, const HGE_Vector_t v2);
+HGE_Vector_t HGE_Vector_Add(const HGE_Vector_t v1, const HGE_Vector_t v2);
+HGE_Vector_t HGE_Vector_Multiply(const HGE_Vector_t v1, float scalar);
+HGE_Vector_t HGE_Vector_Divide(const HGE_Vector_t v1, float scalar);
 
-HGE_Vector *HGE_Vector_SubtractEqual(HGE_Vector *v1, const HGE_Vector v2);
-HGE_Vector *HGE_Vector_AddEqual(HGE_Vector *v1, const HGE_Vector v2);
-HGE_Vector *HGE_Vector_MultiplyEqual(HGE_Vector *v1, float scalar);
-HGE_Vector *HGE_Vector_DivideEqual(HGE_Vector *v1, float scalar);
-BOOL HGE_Vector_Equal(const HGE_Vector v1, const HGE_Vector v2);
-BOOL HGE_Vector_NotEqual(const HGE_Vector v1, const HGE_Vector v2);
+HGE_Vector_t *HGE_Vector_SubtractEqual(HGE_Vector_t *v1, const HGE_Vector_t v2);
+HGE_Vector_t *HGE_Vector_AddEqual(HGE_Vector_t *v1, const HGE_Vector_t v2);
+HGE_Vector_t *HGE_Vector_MultiplyEqual(HGE_Vector_t *v1, float scalar);
+HGE_Vector_t *HGE_Vector_DivideEqual(HGE_Vector_t *v1, float scalar);
+BOOL HGE_Vector_Equal(const HGE_Vector_t v1, const HGE_Vector_t v2);
+BOOL HGE_Vector_NotEqual(const HGE_Vector_t v1, const HGE_Vector_t v2);
 
-float HGE_Vector_Dot(const HGE_Vector *v1, const HGE_Vector *v2);
-float HGE_Vector_Length(const HGE_Vector *v1);
-float HGE_Vector_Angle(const HGE_Vector *v1, const HGE_Vector *v2);
+float HGE_Vector_Dot(const HGE_Vector_t *v1, const HGE_Vector_t *v2);
+float HGE_Vector_Length(const HGE_Vector_t *v1);
+float HGE_Vector_Angle(const HGE_Vector_t *v1, const HGE_Vector_t *v2);
 
-void HGE_Vector_Clamp(HGE_Vector *v, const float max);
-HGE_Vector *Normalize(HGE_Vector *v);
-HGE_Vector *Rotate(HGE_Vector *v, float a);
+void HGE_Vector_Clamp(HGE_Vector_t *v, const float max);
+HGE_Vector_t *Normalize(HGE_Vector_t *v);
+HGE_Vector_t *Rotate(HGE_Vector_t *v, float a);
 
 #ifdef __cplusplus
 }
