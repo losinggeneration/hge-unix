a863534348b0966325af58053217168f12749fb9
Harley Laue <losinggeneration@gmail.com>
2014-07-26 04:41:42 -0500

    Made the switch to userdata for hge
    * Made add_garbage more general as add_meta_function.

--- c_api/src/lua/hge.c
+++ c_api/src/lua/hge.c
@@ -2,6 +2,10 @@
 
 #include "hge_lua.h"
 
+typedef struct {
+	HGE_t *hge;
+} hge_t;
+
 void register_resource(lua_State *L);
 void register_ini(lua_State *L);
 void register_random(lua_State *L);
@@ -15,115 +19,211 @@ void register_gfx(lua_State *L);
 void register_target(lua_State *L);
 void register_texture(lua_State *L);
 
+#define DEBUG_STACK(L) do { printf("stack: %s:%d => %d\n", __FILE__, __LINE__, lua_gettop(L)); } while(0)
+
 inline void add_function(lua_State *L, const char *name, lua_CFunction f) {
 	lua_pushstring(L, name);
 	lua_pushcfunction(L, f);
 	lua_settable(L, -3);
 }
 
-inline void add_garbage(lua_State *L, lua_CFunction f) {
-	if(lua_getmetatable(L, -3) == 0) {
-		lua_newtable(L);
+inline void add_meta_function(lua_State *L, const char *metatable, const char *metafunction, lua_CFunction f) {
+	luaL_newmetatable(L, metatable);
+	add_function(L, metafunction, f);
+	lua_setmetatable(L, -2);
+}
+
+#define add_garbage(L, m, f) add_meta_function(L, m, "__gc", f)
+#define add_index(L, m, f) add_meta_function(L, m, "__index", f)
+#define add_newindex(L, m, f) add_meta_function(L, m, "__newindex", f)
+#define add_tostring(L, m, f) add_meta_function(L, m, "__tostring", f)
+
+/* Check the first argument is userdata and return it */
+hge_t *hge_param_check(lua_State *L) {
+	if(lua_isuserdata(L, 1) == 0) {
+		lua_pushstring(L, "Expected type(hge)");
+		lua_error(L);
+		return NULL;
 	}
 
-	add_function(L, "__gc", f);
+	return (hge_t *)lua_touserdata(L, 1);
+}
 
-	lua_setmetatable(L, -2);
+/* check that the first argument is a userdata & make sure it's not free'd */
+hge_t *hge_check(lua_State *L) {
+	hge_t *h = hge_param_check(L);
+
+	if(h == NULL || h->hge == NULL) {
+		lua_pushstring(L, "Cannot use a free'd type(hge)");
+		lua_error(L);
+		return NULL;
+	}
+
+	return h;
 }
 
 /* void HGE_Release(HGE_t *hge); */
 int system_release(lua_State *L) {
-	printf("free: system_release\n");
+	hge_t *h = hge_param_check(L);
+
+	if(h != NULL) {
+		if(h->hge != NULL) {
+			HGE_Release(h->hge);
+			h->hge = NULL;
+		}
+		h = NULL;
+	}
 	return 0;
 }
 
 /* BOOL HGE_System_Initiate(HGE_t *hge); */
 int system_initiate(lua_State *L) {
-	return 0;
+	hge_t *h = hge_check(L);
+	BOOL b = HGE_System_Initiate(h->hge);
+
+	lua_pushboolean(L, b);
+
+	return 1;
 }
 
 /* void HGE_System_Shutdown(HGE_t *hge); */
 int system_shutdown(lua_State *L) {
+	hge_t *h = hge_check(L);
+	HGE_System_Shutdown(h->hge);
+
 	return 0;
 }
 
 /* BOOL HGE_System_Start(HGE_t *hge); */
 int system_start(lua_State *L) {
-	return 0;
+	hge_t *h = hge_check(L);
+	BOOL b = HGE_System_Start(h->hge);
+
+	lua_pushboolean(L, b);
+
+	return 1;
 }
 
 /* const char* HGE_System_GetErrorMessage(HGE_t *hge); */
 int system_get_error_message(lua_State *L) {
-	return 0;
+	hge_t *h = hge_check(L);
+	const char *err = HGE_System_GetErrorMessage(h->hge);
+
+	lua_pushstring(L, err);
+
+	return 1;
 }
 
 /* void HGE_System_Log(HGE_t *hge, const char *format, ...); */
 int system_log(lua_State *L) {
+	hge_t *h = hge_check(L);
 	return 0;
 }
 
 /* BOOL HGE_System_Launch(HGE_t *hge, const char *url); */
 int system_launch(lua_State *L) {
+	hge_t *h = hge_check(L);
 	return 0;
 }
 
 /* void HGE_System_Snapshot(HGE_t *hge, const char *filename); */
 int system_snapshot(lua_State *L) {
+	hge_t *h = hge_check(L);
 	return 0;
+}
+
+void system_set_state_check(lua_State *L) {
+	if(lua_gettop(L) != 3) {
+		lua_pushstring(L, "Expected three arguments: type(hge), number(state), boolean(value)");
+		lua_error(L);
+	}
 }
 
 /* void HGE_System_SetStateBool(HGE_t *hge, HGE_BoolState_t state, BOOL value); */
 int system_set_state_bool(lua_State *L) {
+	hge_t *h = hge_check(L);
+
+	system_set_state_check(L);
+
+	HGE_BoolState_t state = lua_tonumber(L, 2);
+	BOOL value = lua_toboolean(L, 3);
+
+	HGE_System_SetStateBool(h->hge, state, value);
+
 	return 0;
 }
 
 /* void HGE_System_SetStateFunc(HGE_t *hge, HGE_FuncState_t state, HGE_Callback value); */
 int system_set_state_func(lua_State *L) {
+	hge_t *h = hge_check(L);
 	return 0;
 }
 
 /* void HGE_System_SetStateHwnd(HGE_t *hge, HGE_HwndState_t state, HWND value); */
 int system_set_state_hwnd(lua_State *L) {
+	hge_t *h = hge_check(L);
 	return 0;
 }
 
 /* void HGE_System_SetStateInt (HGE_t *hge, HGE_IntState_t state, int value); */
 int system_set_state_int(lua_State *L) {
+	hge_t *h = hge_check(L);
+	system_set_state_check(L);
+
+	HGE_IntState_t state = lua_tonumber(L, 2);
+	lua_Number value = lua_tonumber(L, 3);
+
+	HGE_System_SetStateInt(h->hge, state, value);
+
 	return 0;
 }
 
 /* void HGE_System_SetStateString(HGE_t *hge, HGE_StringState_t state, const char *value); */
 int system_set_state_string(lua_State *L) {
+	hge_t *h = hge_check(L);
+	system_set_state_check(L);
+
+	HGE_StringState_t state = lua_tonumber(L, 2);
+	const char *value = lua_tostring(L, 3);
+
+	HGE_System_SetStateString(h->hge, state, value);
+
 	return 0;
 }
 
 /* BOOL HGE_System_GetStateBool(HGE_t *hge, HGE_BoolState_t state); */
 int system_get_state_bool(lua_State *L) {
+	hge_t *h = hge_check(L);
 	return 0;
 }
 
 /* HGE_Callback HGE_System_GetStateFunc(HGE_t *hge, HGE_FuncState_t state); */
 int system_get_state_func(lua_State *L) {
+	hge_t *h = hge_check(L);
 	return 0;
 }
 
 /* HWND HGE_System_GetStateHwnd(HGE_t *hge, HGE_HwndState_t state); */
 int system_get_state_hwnd(lua_State *L) {
+	hge_t *h = hge_check(L);
 	return 0;
 }
 
 /* int HGE_System_GetStateInt(HGE_t *hge, HGE_IntState_t state); */
 int system_get_state_int(lua_State *L) {
+	hge_t *h = hge_check(L);
 	return 0;
 }
 
 /* const char* HGE_System_GetStateString(HGE_t *hge, HGE_StringState_t state); */
 int system_get_state_string(lua_State *L) {
+	hge_t *h = hge_check(L);
 	return 0;
 }
 
 luaL_Reg hge_reg[] = {
 	{ "free", system_release },
+	{ "initiate", system_initiate },
 	{ "shutdown", system_shutdown },
 	{ "start", system_start },
 	{ "get_error_message", system_get_error_message },
@@ -134,6 +234,7 @@ luaL_Reg hge_reg[] = {
 	{ "set_state_func", system_set_state_func },
 	{ "set_state_hwnd", system_set_state_hwnd },
 	{ "set_state_int", system_set_state_int },
+	{ "set_state_string", system_set_state_string },
 	{ "get_state_bool", system_get_state_bool },
 	{ "get_state_func", system_get_state_func },
 	{ "get_state_hwnd", system_get_state_hwnd },
@@ -142,10 +243,27 @@ luaL_Reg hge_reg[] = {
 	NULL,
 };
 
+int hge_tostring(lua_State *L) {
+	hge_check(L);
+	lua_pushstring(L, "type(hge)");
+	return 1;
+}
+
 /* HGE_t* HGE_Create(int ver); */
 int hge_create(lua_State *L) {
+	hge_t *hge = (hge_t *) lua_newuserdata(L, sizeof(hge_t));
+	hge->hge = HGE_Create(HGE_VERSION);
+
+	add_garbage(L, "hge.hge", system_release);
+	add_tostring(L, "hge.hge", hge_tostring);
+
+	return 1;
+}
+
+void register_hge(lua_State *L) {
 	lua_newtable(L);
-	luaL_register(L, NULL, hge_reg);
+	// Add the new function to the table
+	add_function(L, "new", hge_create);
 
 	register_resource(L);
 	register_ini(L);
@@ -160,16 +278,15 @@ int hge_create(lua_State *L) {
 	register_target(L);
 	register_texture(L);
 
-	add_garbage(L, system_release);
+	// Setup the hge metatable for the userdata
+	luaL_newmetatable(L, "hge.hge");
+	lua_pushstring(L, "__index");
+	// push the metatable
+	lua_pushvalue(L, -2);
+	lua_settable(L, -3); // metatable[__index] = metatable
+	luaL_register(L, NULL, hge_reg);
 
-	return 1;
-}
-
-void register_hge(lua_State *L) {
-	lua_newtable(L);
-
-	// Add the new function to the table
-	add_function(L, "new", hge_create);
+	lua_setmetatable(L, -2); // setmetatable(hge, metatable)
 
 	// Pop's the hge table off the stack
 	lua_setglobal(L, "hge");
@@ -226,7 +343,7 @@ int resource_load(lua_State *L) {
 	lua_newtable(L);
 	luaL_register(L, NULL, resource_reg);
 
-	add_garbage(L, resource_free);
+	add_garbage(L, "hge.resource", resource_free);
 
 	return 1;
 }
@@ -376,7 +493,7 @@ int effect_load(lua_State *L) {
 	lua_newtable(L);
 	luaL_register(L, NULL, effect_reg);
 
-	add_garbage(L, effect_free);
+	add_garbage(L, "hge.effect", effect_free);
 
 	return 1;
 }
@@ -465,7 +582,7 @@ int music_load(lua_State *L) {
 	lua_newtable(L);
 	luaL_register(L, NULL, music_reg);
 
-	add_garbage(L, music_free);
+	add_garbage(L, "hge.music", music_free);
 
 	return 1;
 }
@@ -500,7 +617,7 @@ int stream_load(lua_State *L) {
 	lua_newtable(L);
 	luaL_register(L, NULL, stream_reg);
 
-	add_garbage(L, stream_free);
+	add_garbage(L, "hge.stream", stream_free);
 
 	return 1;
 }
@@ -783,7 +900,7 @@ int target_create(lua_State *L) {
 	lua_newtable(L);
 	luaL_register(L, NULL, target_reg);
 
-	add_garbage(L, target_free);
+	add_garbage(L, "hge.target", target_free);
 
 	return 1;
 }
@@ -837,7 +954,7 @@ void texture_new(lua_State *L) {
 	lua_newtable(L);
 	luaL_register(L, NULL, texture_instance_reg);
 
-	add_garbage(L, texture_free);
+	add_garbage(L, "hge.texture", texture_free);
 }
 
 /* HTEXTURE HGE_Texture_Create(HGE_t *hge, int width, int height); */
